3c12606e05a69e31914fc60030e77960a5610ce8
duygusahan <dsahan19@posta.pau.edu.tr>
2024-09-25 13:26:20 +0300

    ...

---
+++ SignalRWebUi/Controllers/BookATableController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Versioning;
+using SignalRProject.DtoLayer.BookingDtos;
+using System.Text;
+
+namespace SignalRWebUi.Controllers
+{
+    public class BookATableController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookATableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingDto createBookingDto)
+        {
+            var client =_httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData , Encoding.UTF8 , "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7009/api/Booking", stringContent);
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("CreateBooking");
+            }
+            return View();
+        }
+    }
+}
