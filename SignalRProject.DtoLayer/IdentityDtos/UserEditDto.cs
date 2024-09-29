ffbb7ff7a1810c485b4aea1c5732526322a2c5bd
duygusahan <dsahan19@posta.pau.edu.tr>
2024-09-29 23:24:32 +0300

    ...

---
+++ SignalRProject.DtoLayer/IdentityDtos/UserEditDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRProject.DtoLayer.IdentityDtos
+{
+    public class UserEditDto
+    {
+        public string Mail { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Confirmpassword { get; set; }
+    }
+}
